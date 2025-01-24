0c801328bc791d0239781edac9a9ac0a5835e5fe
Fozilov Meroj <fozilovmerodz@gmail.com>
2025-01-24 22:47:15 +0500

    footer & header

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -3,7 +3,13 @@
 </script>
 
 <template>
-  <NLayoutFooter bordered class="py-4 bg-transparent mx-4">Chengfu Road</NLayoutFooter>
+  <NLayoutFooter bordered class="py-4 bg-transparent mx-4 flex justify-between">
+    <div>Fozilov Meroj</div>
+    <div>
+      © INNOVA -
+      {{ $dayjs().format("YYYY") }}
+    </div>
+  </NLayoutFooter>
 </template>
 
 <style scoped>
--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -5,11 +5,32 @@ const extraDataStore = useExtraDataStore();
 </script>
 
 <template>
-  <NLayoutHeader class="rounded-xl p-4 m-4 shadow-sm">
-    <div class="flex justify-between">
-      <div class="flex items-center">
-        <Icon v-if="extraDataStore.internetOnline" name="tabler:wifi" size="24" class="text-green-500"/>
-        <Icon v-else name="solar:wifi-off" class="mr-2"/>
+  <NLayoutHeader class="rounded-xl shadow-sm px-4 py-2 m-4 w-auto">
+    <div class="flex justify-between items-center">
+      <div class="flex items-center gap-3">
+        <Icon v-tippy="`Online`" v-if="extraDataStore.internetOnline" name="tabler:wifi" size="24"
+              class="text-green-500"/>
+        <Icon v-else name="solar:wifi-off" class="mr-2" size="24"/>
+        <NInput placeholder="Search..." style="min-width: 250px">
+          <template #suffix>
+            <Icon name="tabler:search"/>
+          </template>
+        </NInput>
+      </div>
+      <div class="flex items-center gap-3">
+        <NButton text>
+          <Icon name="tabler:inbox" size="24"/>
+        </NButton>
+        <NButton text class="focus:!text-yellow-600 hover:!text-yellow-500">
+          <Icon name="tabler:bell" size="24"/>
+        </NButton>
+        <div class="flex items-center gap-2">
+          <Icon name="solar:user-circle-outline" size="24"/>
+          <div>
+            <div class="font-bold">Fmeroj</div>
+            <div class="opacity-75 text-xs">Super-Admin</div>
+          </div>
+        </div>
       </div>
     </div>
   </NLayoutHeader>
